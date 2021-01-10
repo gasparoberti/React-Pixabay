e87ef70da2c8b480493583152c707922ce6b06c3
Gaspar <obertigaspar@gmail.com>
2021-01-10 15:41:07 -0300

    se modificó el placeholder del buscador y el tamaño del h en el título del jumbotron

--- src/App.js
+++ src/App.js
@@ -1,26 +1,90 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Buscador from './componentes/Buscador';
+import Resultado from './componentes/Resultado';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    termino : '',
+    imagenes : [], 
+    pagina : ''
+  }
+
+  scroll = () => {
+    const elemento= document.querySelector('.jumbotron');
+    elemento.scrollIntoView('smooth', 'start');
+
+  }
+
+  paginaAnterior = () => {
+    let pagina = this.state.pagina;
+    
+    if (pagina===1) return null;
+
+    pagina--;
+
+    this.setState({pagina},
+      () => {
+        this.consultarApi();
+        this.scroll();
+      });
+
+    // console.log(pagina);
+  }
+  
+  paginaSiguiente = () => {
+    let pagina = this.state.pagina;
+    
+    pagina++;
+
+    this.setState({pagina},
+      () => {
+        this.consultarApi();
+        this.scroll();
+      });
+
+    // console.log(pagina);
+  }
+
+  datosBusqueda= (termino) => {
+    // console.log(termino);
+    this.setState({
+      termino : termino,
+      pagina : 1
+    }, () => {
+      this.consultarApi();
+    })
+  }
+
+  consultarApi = () => {
+    const termino = this.state.termino;
+    const pagina = this.state.pagina;
+    const url= `https://pixabay.com/api/?key=15263025-4b92212a1a574cf596a245571&q=${termino}&per_page=30&page=${pagina}`;
+    // console.log(url);
+    fetch(url)
+    .then(respuesta => respuesta.json() )
+    .then(resultado => this.setState({ imagenes : resultado.hits}) )
+  }
+
+  render(){
+    return (
+      <div className="app container">
+        <div className="jumbotron">
+          <h2 className="text-center">Buscador de imágenes</h2>
+          <br/>
+          <Buscador
+            datosBusqueda={this.datosBusqueda}/>
+        </div>
+        <div className="row justify-content-center">
+            <Resultado
+              imagenes = {this.state.imagenes}
+              paginaAnterior = {this.paginaAnterior}
+              paginaSiguiente = {this.paginaSiguiente}
+            />
+        </div>
+        
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/componentes/Buscador.js
@@ -0,0 +1,31 @@
+import React,{Component} from 'react';
+
+class Buscador extends Component {
+    busquedaref= React.createRef();
+    obtenerDatos= (e)=>{
+        // tomamos el valor del input
+        e.preventDefault();
+        // lo enviamos al componenteprincipal
+        this.props.datosBusqueda(this.busquedaref.current.value);
+    }
+
+    render() {
+        return(
+            <form onSubmit={this.obtenerDatos}>
+                <div className="row">
+                    
+                    <div className="form-group col-md-8">
+                        <input ref={this.busquedaref} type="text" className="form-control form-control-lg" placeholder="Busca tu imagen"/>
+                    </div>
+
+                    <div className="form-group col-md-4">
+                        <input type="submit" className="btn btn-lg btn-danger btn-block" value="Buscar"/>
+                    </div>
+
+                    </div>    
+            </form>
+        );
+    }
+}
+
+export default Buscador;
